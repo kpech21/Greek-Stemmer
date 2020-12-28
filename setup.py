ee91cf323acfb5b3e1157b21cd53562e263084b0
kpechlivanis <kpechlivanis@convertgroup.com>
2020-12-28 22:09:42 +0200

    [chore]: include setup.py for the installation of the libary
    [chore]: include requirements.txt for required pips

---
+++ setup.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import sys
+
+from setuptools import setup, find_packages, Command
+from shutil import rmtree
+
+# TODO: * complete upload command - upload package to PyPI using Twine
+# setup.py structure was based on: https://github.com/kennethreitz/setup.py/blob/master/setup.py
+
+# Package meta-data
+NAME = 'greek-stemmer'
+DESCRIPTION = 'A greek stemmer that utilizes Part of Speech (PoS) tags'
+URL = 'https://github.com/kpech21/Greek-Stemmer'
+EMAIL = 'kpechlivanis21@gmail.com'
+AUTHOR = 'Konstantinos Pechlivanis'
+REQUIRES_PYTHON = '>=3.8'
+VERSION = '1.0.0'
+
+# What packages are required for this module to be executed?
+REQUIRED = ['pytest', 'pytest-cov']
+
+# What packages are optional?
+EXTRAS = {}
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    _license = f.read()
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace('-', '_').replace(' ', '_')
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=['tests', 'docs']),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='GNU License',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
